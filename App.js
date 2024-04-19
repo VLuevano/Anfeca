3dda75360110a966ab221c49e7224cd294d26f49
Victor Luevano <victorluevmon@gmail.com>
2024-04-18 21:53:19 -0600

    Nuevo proyecto con la pantalla principal

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity } from 'react-native';
 
-export default function App() {
+export default function PantallaInicio() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require('./Recursos/Imágenes/FondoInicio.jpg')}
+      style={styles.container}
+    >
+      <View style={styles.logoContainer}>
+        <Image
+          source={require('./Recursos/Imágenes/VITA3.png')}
+          style={styles.logo}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Crear cuenta</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Iniciar sesión</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    resizeMode: 'cover', // Ajustar imagen de fondo
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 50,
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain', // Ajustar tamaño del logo
+  },
+  buttonContainer: {
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#F8DD6C', // Color del botón primario
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: '#000',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
