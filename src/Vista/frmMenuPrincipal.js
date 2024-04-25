4796e9f30c7434cc08b7011481425e821657badd
Victor Luevano <victorluevmon@gmail.com>
2024-04-25 00:53:03 -0600

    Se cambió el menú principal

--- src/Vista/frmMenuPrincipal.js
+++ src/Vista/frmMenuPrincipal.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { View, Text } from "react-native"
 
-const HomeScreen = ({ userData }) => {
+const HomeScreen = () => {
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ fontSize: 30, textAlign: "center" }}>Bienvenido, {userData.name}</Text>
-            <Text style={{ fontSize: 20, textAlign: "center" }}>Correo electrónico: {userData.email}</Text>
-            {/* Mostrar otros detalles del usuario aquí */}
+        <View>
+            <Text
+                style={{
+                    fontSize: 30,
+                    textAlign: "center",
+                    marginTop: "20%",
+
+                }}
+            >Home Screen</Text>
+
         </View>
     );
 }
