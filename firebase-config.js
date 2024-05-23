2d5d58dc98ea80480c1f4f04bfdcaab4fcfd278f
Victor Luevano <victorluevmon@gmail.com>
2024-05-23 11:57:22 -0600

    Se solucionó un error con firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,8 @@
+// Importa los módulos necesarios de Firebase Auth y AsyncStorage
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeApp } from 'firebase/app';
+
 // Your web app's Firebase configuration
 export const firebaseConfig = {
   apiKey: "AIzaSyDVeUknRNfk1FwkGO1YVPkiTujAPoXzpxE",
@@ -7,3 +12,11 @@ export const firebaseConfig = {
   messagingSenderId: "283985634560",
   appId: "1:283985634560:web:85c5a997f78d499dd27fe5"
 };
+
+// Inicializa la aplicación de Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializa Firebase Auth con AsyncStorage
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
